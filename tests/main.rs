fb5e6a23d7d1562127aa93dc2cb57cce1dfe2068
Daya Adianto <addianto@users.noreply.github.com>
2024-04-30 06:44:42 +0700

    Add failing test for multiple URLs

--- tests/main.rs
+++ tests/main.rs
@@ -13,3 +13,17 @@ fn debug_flag_single_url() {
         .assert()
         .stdout(predicates::str::contains("url: https://google.com"));
 }
+
+#[test]
+fn debug_flag_multiple_urls() {
+    // Setup
+    let mut command: Command = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+
+    // Exercise
+    let assert = command.args(&["-d", "https://google.com", "https://github.com"]);
+
+    // Verify
+    assert
+        .assert()
+        .stdout(predicates::str::contains("url: https://google.com https://github.com"));
+}
