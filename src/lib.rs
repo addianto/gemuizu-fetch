869411927d019ed7693ac13312fbd2ef5bb36c37
Daya Adianto <addianto@users.noreply.github.com>
2024-05-01 18:22:49 +0700

    Implement generate_file_name

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,20 @@
-/// Generate a unique file name based on the given URL
-pub fn generate_file_name(_url: &str) -> String {
-    String::from("TODO").to_string()
+/// Generates a unique file name based on the given URL.
+///
+/// The generated file name is formatted as `<domain>_<game>_<identifier>.html`.
+///
+/// Example:
+///
+/// ```
+/// let filename = gemuizu_fetch::generate_file_name("https://gamewith.net/palworld/43489");
+/// assert_eq!("gamewith.net_palworld_43489.html", filename);
+/// ```
+pub fn generate_file_name(url: &str) -> String {
+    let url_parts: Vec<&str> = url.split('/').collect();
+    let length = url_parts.len();
+
+    let domain: &str = url_parts[2];
+    let game: &str = url_parts[3];
+    let identifier: &str = url_parts[length - 1];
+
+    format!("{}_{}_{}.html", domain, game, identifier)
 }
